a7bbf293bf3ff2fe8c5dbe04ea939c5e2cbc2742
Samuel Slade <samuel.pjm.slade@live.co.uk>
2018-12-18 04:45:08 +0000

    Changed port numbers for hosting of APIs

--- src/Banking.Api.Queries/Program.cs
+++ src/Banking.Api.Queries/Program.cs
@@ -19,6 +19,7 @@ namespace Banking.Api.Queries
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5500", "https://localhost:5501")
                 .UseStartup<Startup>();
     }
 }
